786d8beb1a5dabc250f98fe57226fc2156db5ad4
Monotek <timur.yakhin.99@gmail.com>
2023-04-23 10:43:31 +0300

    валидация 1  формы done

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const closePopupButtonList = document.querySelectorAll('.popup__button_close'),
     editProfileButton = document.querySelector('.profile__edit-button'),
     profileName = document.querySelector('.profile__name'),
     profileProfi = document.querySelector('.profile__profi'),
-    popupIput = popupForm.querySelector('.popup__input'),
+    popupInput = popupForm.querySelectorAll('.popup__input'),
     formEditProfile = document.querySelector('#formEditProfile'),
     addCardButton = document.querySelector('.profile__add-button'),
     elementsContainer = document.querySelector('.elements'),
@@ -18,7 +18,7 @@ const closePopupButtonList = document.querySelectorAll('.popup__button_close'),
     imagePlaceTitle = document.querySelector('.popup__title_image'),
     placeName = document.getElementById('inputPlaceName'),
     placeHref = document.getElementById('inputPlaceHref'),
-    popupError = popupForm.querySelector(`.${popupIput.id}-error`),
+    inputError = popupForm.querySelector(`.${popupInput[0].id}-error`),
     initialCards = [{
             name: 'Архыз',
             link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
@@ -79,7 +79,7 @@ function increaseImage(srcValue, nameValue) {
 
 function addElement(srcValue, nameValue) {
     const element = elementsTemplate.querySelector('.element').cloneNode(true),
-    elementImage = element.querySelector('.element__image');
+        elementImage = element.querySelector('.element__image');
     elementImage.src = srcValue;
     elementImage.alt = nameValue;
     element.querySelector('.element__attractions').textContent = nameValue;
@@ -99,23 +99,6 @@ function addElement(srcValue, nameValue) {
 function renderCard(srcValue, nameValue) {
     elementsContainer.prepend(addElement(srcValue, nameValue));
 };
-function showInputError(element, errorMessage){
-    element.classList.add('popup__input_type_error');
-    popupError.textContent = errorMessage;
-    popupError.classList.add('popup__input-error_active');
-};
-function hideInputError(element){
-    element.classList.remove('popup__input_type_error');
-    popupError.textContent = ' ';
-    popupError.classList.remove('popup__input-error_active');
-};
-function isValid(){
-    if(!popupIput.validity.valid){
-        showInputError(popupIput, popupIput.validationMessage);
-    } else {
-        hideInputError(popupIput);
-    }
-}
 formEditProfile.addEventListener('submit', handleEditProfileFormSubmit);
 closePopupButtonList.forEach(element => element.addEventListener('click', () => {
     closePopup(document.querySelector('.popup_opened'));
@@ -131,4 +114,22 @@ popupAddCard.addEventListener('submit', (e) => {
     e.target.reset();
     closePopup(popupAddCard);
 });
-popupIput.addEventListener('input', isValid);
+/* Валидация */
+function showInputError(element, errorMessage){
+    element.classList.add('popup__input_type_error');
+    inputError.textContent = errorMessage;
+    inputError.classList.add('popup__input-error_active');
+}
+function hideInputError(element){
+    element.classList.remove('popup__input_type_error');
+    inputError.classList.remove('popup__input-error_active');
+    inputError.textContent = ' ';
+}
+function isValid(){
+    if(!popupInput[0].validity.valid){
+        showInputError(popupInput[0], popupInput[0].validationMessage);
+    }else{
+        hideInputError(popupInput[0]);
+    }
+}
+popupInput[0].addEventListener('input', isValid);
